132ad75b87faa0f39f88a22fa39014eb0b989ee1
tsheporamantso <tshepo.ramantso@outlook.com>
2025-07-29 07:59:42 +0200

    Fix loading state handling and improve error message in FirstRequest component

--- src/components/FirstRequest.jsx
+++ src/components/FirstRequest.jsx
@@ -1,17 +1,21 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-
+import Loading from "../Pages/Loading";
 const url = "https://www.course-api.com/react-store-products";
 
 const FirstRequest = () => {
   const [products, setProducts] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   const fetchProducts = async () => {
+    setIsLoading(true);
     try {
       const resp = await axios.get(url);
       setProducts(resp.data);
+      setIsLoading(false);
     } catch (error) {
-      throw new Error(`Something went wrong ${error.resp}`);
+      setIsLoading(true);
+      throw new Error(`Something went wrong ${error.resp.data}`);
     }
   };
 
@@ -19,9 +23,29 @@ const FirstRequest = () => {
     fetchProducts();
   }, []);
 
-  console.log(products);
+  if (isLoading) {
+    return (
+      <main>
+        <Loading />
+      </main>
+    );
+  }
 
-  return <div>FirstRequest</div>;
+  return (
+    <div>
+      {products.map((item) => {
+        return (
+          <article key={item.id}>
+            <img
+              style={{ width: "250px", height: "250px" }}
+              src={item.image}
+              alt={item.name}
+            />
+          </article>
+        );
+      })}
+    </div>
+  );
 };
 
 export default FirstRequest;
