9edebe2dbefa34d6721ccb3778262f221721513f
tsheporamantso <tshepo.ramantso@outlook.com>
2025-07-29 11:14:38 +0200

    Update import path for Setup component and add ToastContainer for notifications

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import Setup from "./components/Headers";
-
+import Setup from "./components/PostRequest";
+import { ToastContainer } from "react-toastify";
 function App() {
   return (
     <main>
       <Setup />
+      <ToastContainer position="top-center" />
     </main>
   );
 }
