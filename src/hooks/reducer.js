bb3d6f90542dcbd44e6a58e0bd04baae30f76705
tsheporamantso <tshepo.ramantso@outlook.com>
2025-08-08 10:27:21 +0200

    Add GET_PRODUCTS action type to reducer for handling product data

--- src/hooks/reducer.js
+++ src/hooks/reducer.js
@@ -1,4 +1,4 @@
-import { SET_ISLOADING, GET_USERS } from "./actions";
+import { SET_ISLOADING, GET_USERS, GET_PRODUCTS } from "./actions";
 
 const reducer = (state, action) => {
   if (action.type === SET_ISLOADING) {
@@ -6,6 +6,9 @@ const reducer = (state, action) => {
   }
   if (action.type === GET_USERS) {
     return { ...state, users: action.payload, isLoading: false };
+  }
+  if (action.type === GET_PRODUCTS) {
+    return { ...state, products: action.payload, isLoading: false };
   }
   throw new Error(`No matching ${action.type} - action type`);
 };
